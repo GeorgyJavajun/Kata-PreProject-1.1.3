664f284b1083c884746d8eaed2d69ad8a9e9e863
GeorgyJavajun <103282860+GeorgyJavajun@users.noreply.github.com>
2022-06-19 09:47:39 +0300

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,7 +2,7 @@ package jm.task.core.jdbc.util;
 import java.sql.*;
 public class Util {
     private static final String USER = "root";
-    private static final String PASSWORD = "PisunSQL278";
+    private static final String PASSWORD = "password278";
     private static final String URL = "jdbc:mysql://localhost:3306/mysql";
 
     public static Connection getConnect() {
